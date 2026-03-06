b1f99ad0082b217e6ba03866259429705ecfd18b
Leonid <leonilfilippenkov@gmail.com>
2026-03-06 19:32:11 +0200

    update parser.h

--- src/CSV/parser.h
+++ src/CSV/parser.h
@@ -2,7 +2,6 @@
 #include "struct.h"
 #include <ctype.h>
 #include <string.h>
-#include <stdbool.h>
 
 FieldType detectType(char* str);
 Field* makeField(char* line, size_t end, size_t start, size_t indexCol);
